0e906997ae3ae8fa088cd748fd42675166433b4b
dip <deepcharhate2007@gmail.com>
2025-03-08 01:11:24 +0530

    describe the change

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -19,7 +19,7 @@ const Results = () => {
       name: "Dip Charhate",
       achievement: "12th Board 91.5%",
       year: "2023",
-      image: "#"
+      image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
     }
   ];
 
